af1027d42e246d497d6cdecd03a58f1d01332aa1
a568261070 <woshichenhong12@gmail.com>
2019-03-07 19:29:48 +0800

    CSS part2

--- js/demo.js
+++ js/demo.js
@@ -7,6 +7,12 @@ function timeCount() {
 
     setTimeout("timeCount()",500);//每隔500MS 进行一次
 
+
+
 }
 
 timedCount();
+
+
+//隐藏淘宝webview标签
+document.getElementsByClassName("rmsp rmsp-bl rmsp-bl")[0].style.display="none";
